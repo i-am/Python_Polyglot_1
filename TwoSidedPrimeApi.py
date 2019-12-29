8794099a37a920f09a6156125f66241d89cc7b4d
i-am <akshaymorey001@gmail.com>
2019-12-29 16:16:29 +0530

    redirect fot invalid api

--- TwoSidedPrimeApi.py
+++ TwoSidedPrimeApi.py
@@ -1,5 +1,5 @@
 import flask
-from flask import jsonify
+from flask import jsonify, redirect
 import util
 
 app = flask.Flask(__name__)
@@ -7,10 +7,14 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Two sided prime!</h1><p>/api/&lt;n&rt; to check if n is two sided prime</p>"
+    return "<h1>Two sided prime!</h1><p>http://127.0.0.1:5000/api/&lt;n&gt; to check if n is two sided prime</p>"
 
 @app.route('/api/<int:n>', methods=['GET'])
 def is_two_sided_prime(n):
     return jsonify(util.isTwoSidedPrime(n))
+
+@app.route('/api/<path>')
+def default_redirect(path):
+    return redirect('/')
 
 app.run()
